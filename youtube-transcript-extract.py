dfbb92829edf2ca6c78e55eed01bb21541f2cf2a
AmalrajMorais1802 <58253903+AmalrajMorais1802@users.noreply.github.com>
2020-01-25 22:44:43 +0530

    Modified logic for multiple videos per channel

--- youtube-transcript-extract.py
+++ youtube-transcript-extract.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[20]:
+# In[66]:
 
+
+#Step 1 :
 
 #!pip install apiclient
 #!pip install freeze
@@ -34,17 +36,21 @@ import langdetect
 import re
 
 
-# In[3]:
+# In[67]:
 
 
-DEVELOPER_KEY = "<Insert your API Key>"
+#Step 2 :
+
+DEVELOPER_KEY = "AIzaSyBiTLl0Pis4g7RJCAEwjPobBaqrWW9rHdI"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 from youtube_transcript_api import YouTubeTranscriptApi
 
 
-# In[4]:
+# In[68]:
 
+
+#Step 3 :
 
 def youtube_search(q, max_results=49,order="relevance", token=None, location=None, location_radius=None):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
@@ -113,79 +119,82 @@ def youtube_search(q, max_results=49,order="relevance", token=None, location=Non
     return youtube_dict
 
 
-# In[5]:
+# In[108]:
 
+
+#Step 4:
 
 import os
-os.chdir("<Your Directory>/Youtube Automation Blog")
-
-
-# In[6]:
-
-
-df=pd.DataFrame(youtube_search("Redmi Note 8 pro", max_results=49))
-
-
-# In[7]:
-
-
+os.chdir("C:/Users/antony.morais/Desktop/Amalraj/Youtube Automation Blog")
+df=pd.DataFrame(youtube_search("Samsung Galaxy M30 S", max_results=49))
 df1 = df[['title','videoId','viewCount','channelTitle','channelSubCount','commentCount','likeCount','dislikeCount','tags','favoriteCount','channelId','categoryId']]
 df1.columns = ['Title','VideoId','ViewCount','ChannelTitle','ChannelSubCount','CommentCount','likeCount','dislikeCount','tags','favoriteCount','channelId','categoryId']
 
 
-# In[8]:
+# In[109]:
 
 
-df1.loc[:,'title_translated']=df1['Title']
-df1.loc[:,'title_translated']=df1.apply(lambda row: mtranslate.translate(row.Title,"en","auto"), axis=1)
-#df1.loc[:,'title_translated']=df1.apply(lambda row: str.lower(row.title_translated), axis=1)
+# df1.loc[:,'title_translated']=df1['Title']
+# df1.loc[:,'title_translated']=df1.apply(lambda row: mtranslate.translate(row.Title,"en","auto"), axis=1)
 
 
-# In[9]:
+# In[110]:
 
 
-df1.loc[:,"emv_video"] = df1.apply(lambda row: int(row.ViewCount)*0.14+int(row.CommentCount)*8.20+int(row.likeCount)*0.72, axis=1)
-df1.loc[:,"emv_subscriber"] = df1.apply(lambda row:int(row.ChannelSubCount)*16.54 , axis=1)
+# df1.shape
 
 
-# In[10]:
+# In[111]:
 
 
-df1.shape
+# import urllib.request, json 
+# with urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCOhHO2ICt0ti9KAh-QHvttQ&key=AIzaSyBiTLl0Pis4g7RJCAEwjPobBaqrWW9rHdI") as url:
+#     data = json.loads(url.read().decode())
+#     print(data)
 
 
-# In[11]:
+# In[112]:
 
 
-df1.head(10)
+# from pandas.io.json import json_normalize
+# json_normalize(data['kind'])
 
 
-# In[12]:
+# In[113]:
 
 
-#Retain only one video per channel (channelID column) based on highest value of emv_video column. This is to overcome bias
-df1.sort_values(['emv_video'], ascending=False,inplace=True)
+#Step 5:
+
+#Remove Tutorial Videos
+df1=df1[[not i for i in df1.Title.str.contains("how to|tutorial|getting started")]]
+#Retain only one video per channel (channelID column) based on highest value of Views, Likes and Comments.
+#This is to overcome redundant information.
+df1.sort_values(['ViewCount','likeCount','CommentCount'], ascending=[False,False,False],inplace=True)
 df1.reset_index(drop=True,inplace=True)
-df1_dup=pd.DataFrame(df1.duplicated(subset='channelId', keep='first'))
+# df1_dup=pd.DataFrame(df1.duplicated(subset='channelId', keep='first'))
+# df1_dup.columns=["Dup"]
+# df_red=df1[df1_dup['Dup']==False]
+# df_red.reset_index(drop=True,inplace=True)
+# df_red.shape
+df_red=df1
 
 
-# In[13]:
+# In[114]:
 
 
-df1_dup.columns=["Dup"]
-df_red=df1[df1_dup['Dup']==False]
-df_red.reset_index(drop=True,inplace=True)
 df_red.shape
 
 
-# In[14]:
+# In[115]:
 
 
-df_red=df_red[[not i for i in df_red.title_translated.str.contains("how to","tutorial")]]
-df_red.loc[:,"title_ln"]=df_red.apply(lambda row: langdetect.detect(row.Title), axis=1)
-df_red.shape
+#Step 6:
 
-# In[18]:
+df_red.loc[:,"emv_video"] = df_red.apply(lambda row: int(row.ViewCount)*0.14+int(row.CommentCount)*8.20+int(row.likeCount)*0.72, axis=1)
+#df1.loc[:,"emv_subscriber"] = df1.apply(lambda row:int(row.ChannelSubCount)*16.54 , axis=1)
+
+
+# In[116]:
 
 
 videoid = list(df_red['VideoId'])
@@ -195,32 +204,44 @@ vids_without_sub = x[1]
 df_trans = pd.DataFrame(list(vids_with_sub.keys()), columns=['VideoId'])
 
 
-# In[21]:
+# In[117]:
 
 
 result2= []
 for i in range(0,len(vids_with_sub)):
+    print(i)
     result1 = []
     list_con = list(vids_with_sub.values())[i]
-    for i in list_con:
-        text_proc=i['text']
-        #print(text_proc)
+    for j in list_con:
+        text_proc=j['text']
         if(re.findall('[a-zA-Z]',text_proc)==[]) :
             text_proc_fin=text_proc
+            #print(text_proc_fin)
         else :
             txt_lan=langdetect.detect(text_proc)
             if(txt_lan!='en'):
-                text_proc_fin=mtranslate.translate(text_proc,"en","auto")
+                try :
+                    #print("Translating !!!!!!!!!")
+                    text_proc_fin=mtranslate.translate(text_proc,"en","auto")
+                except :
+                    text_proc_fin=text_proc
+                #print(text_proc_fin)
             else :
                 text_proc_fin=text_proc
+                #print(text_proc_fin)
         result1.append(text_proc_fin)
     result2.append(' '.join(result1))
 
 
-# In[22]:
+# In[118]:
 
 
 df_trans['Transcripts'] = result2
+
+
+# In[119]:
+
+
 fin_df = pd.merge(df_red, df_trans, how='left', left_on='VideoId', right_on='VideoId')
-fin_df=fin_df.drop(['title_ln','categoryId','channelId','ChannelTitle','VideoId','Title','VideoId','tags','favoriteCount'], axis = 1) 
-fin_df.reset_index(drop=True).to_csv("redmi_note_8_pro_final.csv", sep=',', encoding='utf-8',index=False)
+fin_df=fin_df.drop(['categoryId','channelId','ChannelTitle','VideoId','Title','VideoId','tags','favoriteCount'], axis = 1) 
+fin_df.reset_index(drop=True).to_csv("Samsung_Galaxy_M30S.csv", sep=',', encoding='utf-8',index=False)
